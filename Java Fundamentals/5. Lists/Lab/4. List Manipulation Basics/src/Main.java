2a70c77e9e5b20b263767acfec27abbc9998a534
Svetozar Blazhev <nathraichean@abv.bg>
2020-02-19 18:29:19 +0200

    Completed exercises.

--- Java Fundamentals/5. Lists/Lab/4. List Manipulation Basics/src/Main.java
+++ Java Fundamentals/5. Lists/Lab/4. List Manipulation Basics/src/Main.java
@@ -7,7 +7,11 @@
  */
 
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -15,6 +19,55 @@ public class Main {
 
         //init
         Scanner scanner = new Scanner(System.in);
+
+        List<Integer> numbers =
+                Arrays.stream(scanner.nextLine().split(" "))
+                        .map(Integer::parseInt)
+                        .collect(Collectors.toList());
+
+        boolean cycle = true;
+
+        while (cycle){
+
+
+            String inputA = scanner.nextLine();
+            String[] input = inputA.split(" ");
+
+            if (inputA.equals("end")){
+                for (int num: numbers) {
+                    System.out.print(num + " ");
+                }
+                cycle = false;
+            }
+
+            listManipulator(numbers,input);
+
+
+        }
+
+
+    }
+
+    public static void listManipulator(List<Integer> list, String[] input){
+
+        switch (input[0]){
+
+            case "Add":
+                list.add(Integer.parseInt(input[1]));
+                break;
+            case "Remove":
+                list.remove((Object)Integer.parseInt(input[1]));
+                break;
+            case "RemoveAt":
+                list.remove(Integer.parseInt(input[1]));
+                break;
+            case "Insert":
+
+                list.add(Integer.parseInt(input[2]), Integer.parseInt(input[1]));
+                break;
+
+        }
+
 
     }
 }

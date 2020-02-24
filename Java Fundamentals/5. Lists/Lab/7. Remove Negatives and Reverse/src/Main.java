1c39f98becbe82b8774eef6eedfd2e8b46a5862f
Svetozar Blazhev <nathraichean@abv.bg>
2020-02-24 20:51:10 +0200

    JavaFund>Lists>Lab>7.Completed

--- Java Fundamentals/5. Lists/Lab/7. Remove Negatives and Reverse/src/Main.java
+++ Java Fundamentals/5. Lists/Lab/7. Remove Negatives and Reverse/src/Main.java
@@ -7,7 +7,11 @@
  */
 
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -15,6 +19,22 @@ public class Main {
 
         //init
         Scanner scanner = new Scanner(System.in);
+
+        List<Integer> numbers =
+                Arrays.stream(scanner.nextLine().split(" "))
+                        .map(Integer::parseInt)
+                        .collect(Collectors.toList());
+
+        numbers.removeIf(n -> n<0);
+        
+        Collections.reverse(numbers);
+
+        if (numbers.isEmpty()){
+            System.out.println("empty");
+        }
+        else {
+            System.out.println(numbers.toString().replaceAll("[\\[\\],]", ""));
+        }
 
     }
 }

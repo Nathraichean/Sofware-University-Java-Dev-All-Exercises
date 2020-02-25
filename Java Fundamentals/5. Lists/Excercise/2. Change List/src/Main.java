cf3375fe14ab9a2aabc4d6b57321bc37de0b9d83
Svetozar Blazhev <nathraichean@abv.bg>
2020-02-25 23:24:23 +0200

    JavaFund>5.Lists>Exr>2.Complete

--- Java Fundamentals/5. Lists/Excercise/2. Change List/src/Main.java
+++ Java Fundamentals/5. Lists/Excercise/2. Change List/src/Main.java
@@ -7,7 +7,11 @@
  */
 
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -16,5 +20,48 @@ public class Main {
         //init
         Scanner scanner = new Scanner(System.in);
 
+        String init = scanner.nextLine();
+        List<Integer> numbersList;
+
+        if (init.equals("")){
+            numbersList = new ArrayList<>();
+        }
+        else {
+            numbersList =
+                    Arrays.stream(init.split(" "))
+                            .map(Integer::parseInt)
+                            .collect(Collectors.toList());
+        }
+
+
+        boolean endCycle = false;
+
+        while (!endCycle){
+
+            String[] input = scanner.nextLine().split("\\s+");
+
+            listManipulator(numbersList, input);
+
+            if (input[0].equals("end")){
+                System.out.println(numbersList.toString().replaceAll("[\\[\\],]", ""));
+                endCycle = true;
+            }
+        }
+    }
+
+    public static void listManipulator(List<Integer> list, String[] input){
+        switch (input[0]){
+            case "Delete":
+                while (list.contains(Integer.parseInt(input[1]))) list.remove((Object)Integer.parseInt(input[1]));
+                break;
+            case "Insert":
+                if (list.size()==0){
+                    list.add(Integer.parseInt(input[1]));
+                }
+                else if (list.size() > Integer.parseInt(input[2]) && Integer.parseInt(input[2]) >= 0)
+                    list.add(Integer.parseInt(input[2]),Integer.parseInt(input[1]));
+                break;
+        }
     }
 }
+
--- Java Fundamentals/5. Lists/Lab/5. List Manipulation Advanced/src/Main.java
+++ Java Fundamentals/5. Lists/Lab/5. List Manipulation Advanced/src/Main.java
@@ -7,7 +7,11 @@
  */
 
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -15,6 +19,114 @@ public class Main {
 
         //init
         Scanner scanner = new Scanner(System.in);
+
+        List<Integer> numbers =
+                Arrays.stream(scanner.nextLine().split(" "))
+                        .map(Integer::parseInt)
+                        .collect(Collectors.toList());
+
+        boolean cycle = true;
+
+        while (cycle){
+
+
+            String inputA = scanner.nextLine();
+            String[] input = inputA.split(" ");
+
+            if (inputA.equals("end")){
+                cycle = false;
+            }
+
+            listManipulator(numbers,input);
+
+
+        }
+
+
+    }
+
+    public static void listManipulator(List<Integer> list, String[] input){
+
+        switch (input[0]){
+
+            case "Add":
+                list.add(Integer.parseInt(input[1]));
+                break;
+            case "Contains":
+                if (list.contains(Integer.parseInt(input[1])))System.out.println("Yes");
+                else System.out.println("No such number");
+                break;
+            case "Remove":
+                list.remove((Object)Integer.parseInt(input[1]));
+                break;
+            case "RemoveAt":
+                list.remove(Integer.parseInt(input[1]));
+                break;
+            case "Insert":
+                list.add(Integer.parseInt(input[2]), Integer.parseInt(input[1]));
+                break;
+            case "Print":
+                if (input[1].equals("even")){
+                    for (int num: list) {
+                        if (num%2==0){
+                            System.out.print(num + " ");
+                        }
+                    }
+                    System.out.println();
+                }
+                else {
+                    for (int num: list) {
+                        if (num%2>0){
+                            System.out.print(num + " ");
+                        }
+                    }
+                    System.out.println();
+                }
+                break;
+            case "Get":
+                if (input[1].equals("sum")){
+                    int sum = 0;
+                    for (int num : list) {
+                        sum+=num;
+                    }
+                    System.out.println(sum);
+                }
+                break;
+            case "Filter":
+                if (input[1].equals(">=")){
+                    for (int num: list) {
+                        if (num >= Integer.parseInt(input[2])){
+                            System.out.print(num + " ");
+                        }
+                    }
+                    System.out.println();
+                }
+                else if (input[1].equals("<=")){
+                    for (int num: list) {
+                        if (num <= Integer.parseInt(input[2])){
+                            System.out.print(num + " ");
+                        }
+                    }
+                    System.out.println();
+                }
+                else if (input[1].equals(">")){
+                    for (int num: list) {
+                        if (num > Integer.parseInt(input[2])){
+                            System.out.print(num + " ");
+                        }
+                    }
+                    System.out.println();
+                }
+                else if (input[1].equals("<")){
+                    for (int num: list) {
+                        if (num < Integer.parseInt(input[2])){
+                            System.out.print(num + " ");
+                        }
+                    }
+                    System.out.println();
+                }
+        }
+
 
     }
 }

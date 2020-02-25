9d57675540834aadc24bf2ba2f7a55d85f0351d0
Svetozar Blazhev <nathraichean@abv.bg>
2020-02-25 21:08:24 +0200

    JavaFund>Lists>Exer>1.Completed

--- Java Fundamentals/5. Lists/Excercise/1. Train/src/Main.java
+++ Java Fundamentals/5. Lists/Excercise/1. Train/src/Main.java
@@ -7,7 +7,10 @@
  */
 
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -16,5 +19,42 @@ public class Main {
         //init
         Scanner scanner = new Scanner(System.in);
 
+        List<Integer> trainWagons =
+                Arrays.stream(scanner.nextLine().split(" "))
+                        .map(Integer::parseInt)
+                        .collect(Collectors.toList());
+
+        int capacity = Integer.parseInt(scanner.nextLine());
+
+        boolean endCycle = false;
+
+        while (!endCycle){
+
+            String[] input = scanner.nextLine().split(" ");
+
+            if (input.length == 2){
+                if (input[0].equals("Add")) trainWagons.add(Integer.parseInt(input[1]));
+            }
+            else if (input.length == 1){
+                if (input[0].equals("end")){
+                    System.out.println(trainWagons.toString().replaceAll("[\\[\\],]", ""));
+                    endCycle = true;
+                }
+                else{
+                    int people = Integer.parseInt(input[0]);
+                    boolean arePeopleOnWagon = false;
+
+                    for (int wagon: trainWagons) {
+                        if (wagon+people <= capacity && !arePeopleOnWagon){
+                            trainWagons.set(trainWagons.indexOf(wagon), wagon+people);
+                            arePeopleOnWagon = true;
+                        }
+                    }
+                }
+            }
+
+
+
+        }
     }
 }

a64b9e49cb939b527fa586862c1761e7c03691c8
Svetozar Blazhev <nathraichean@abv.bg>
2020-02-28 23:38:26 +0200

    JavaFund>Lists>Exr>3.Completed

--- Java Fundamentals/5. Lists/Excercise/3. House Party/src/Main.java
+++ Java Fundamentals/5. Lists/Excercise/3. House Party/src/Main.java
@@ -7,6 +7,8 @@
  */
 
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -15,6 +17,32 @@ public class Main {
 
         //init
         Scanner scanner = new Scanner(System.in);
+
+        int numberOfPeople = Integer.parseInt(scanner.nextLine());
+        List<String> partyList = new ArrayList<>();
+
+        for (int i = 0; i < numberOfPeople; i++) {
+
+            String[] input = scanner.nextLine().split("\\s+");
+
+            if (input[1].equals("is") && input[2].equals("going!")){
+
+                if (!partyList.contains(input[0])){
+                    partyList.add(input[0]);
+                }
+                else System.out.println(input[0] + " is already in the list!");
+            }
+            else if (input[1].equals("is") && input[2].equals("not")){
+                if (partyList.contains(input[0])){
+                    partyList.remove(input[0]);
+                }
+                else System.out.println(input[0] + " is not in the list!");
+            }
+        }
+
+        for (String name: partyList) {
+            System.out.println(name);
+        }
 
     }
 }

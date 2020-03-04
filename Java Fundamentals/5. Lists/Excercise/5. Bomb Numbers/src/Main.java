017ca4c465bd54a60ee1f3fcd6a132eb97c57dc2
Svetozar Blazhev <nathraichean@abv.bg>
2020-03-04 10:11:52 +0200

    JavaFund>Lists>Exer>6. Completed

--- Java Fundamentals/5. Lists/Excercise/5. Bomb Numbers/src/Main.java
+++ Java Fundamentals/5. Lists/Excercise/5. Bomb Numbers/src/Main.java
@@ -7,7 +7,11 @@
  */
 
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -15,6 +19,49 @@ public class Main {
 
         //init
         Scanner scanner = new Scanner(System.in);
+
+        List<Integer> field =
+                Arrays.stream(scanner.nextLine().split("\\s+"))
+                        .map(Integer::parseInt)
+                        .collect(Collectors.toList());
+
+        String[] bomb = scanner.nextLine().split(" ");
+        int sumUnexploded = 0;
+
+        int bombIndex = Integer.parseInt(bomb[0]);
+        int bombSplash = Integer.parseInt(bomb[1]);
+
+        ArrayList<Integer> explodedIndexesList = new ArrayList<>();
+
+        int indexCounter = 0;
+
+        for (int coordinate : field) {
+            int currentIndex = indexCounter;
+            int fieldSize = field.size();
+
+            if (coordinate == bombIndex) {
+                for (int i = currentIndex; i <= currentIndex+bombSplash; i++) {
+                    if (i < fieldSize)
+                        explodedIndexesList.add(i);
+                }
+                for (int i = currentIndex; i >= currentIndex-bombSplash; i--){
+                    if (i >= 0)
+                        explodedIndexesList.add(i);
+                }
+            }
+            indexCounter++;
+        }
+
+        indexCounter = 0;
+
+        for (int index:field) {
+            if (!explodedIndexesList.contains(indexCounter)){
+                sumUnexploded+=index;
+            }
+            indexCounter++;
+        }
+
+        System.out.println(sumUnexploded);
 
     }
 }

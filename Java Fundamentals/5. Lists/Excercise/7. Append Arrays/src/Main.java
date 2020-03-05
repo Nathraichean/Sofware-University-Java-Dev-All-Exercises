e8991ff024444ed4af1ceb9131c8593efaf54b2f
Svetozar Blazhev <nathraichean@abv.bg>
2020-03-05 18:07:03 +0200

    JavaFund>Lists>Exer>7.Completed

--- Java Fundamentals/5. Lists/Excercise/7. Append Arrays/src/Main.java
+++ Java Fundamentals/5. Lists/Excercise/7. Append Arrays/src/Main.java
@@ -7,6 +7,8 @@
  */
 
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -16,5 +18,33 @@ public class Main {
         //init
         Scanner scanner = new Scanner(System.in);
 
+
+        String input = scanner.nextLine();
+
+        if (!input.equals("")){
+            String[] arrays = input.split("\\|");
+            if (true){
+                ArrayList<Integer> finalList = new ArrayList<>();
+
+                for (int i = arrays.length-1; i >= 0 ; i--) {
+
+                    String[] currentArray = arrays[i].trim().split("\\s+");
+
+                    for (String item: currentArray) {
+                        try {
+                            finalList.add(Integer.parseInt(item));
+                        }
+                        catch (NumberFormatException e){
+                            System.out.println("");
+                        }
+                    }
+
+                }
+
+                for (int num: finalList) {
+                    System.out.print(num + " ");
+                }
+            }
+        }
     }
 }

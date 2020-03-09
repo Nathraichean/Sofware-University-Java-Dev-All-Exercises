8b77a130f6aafdcec7cfede6b9de52e4f36c88f4
Svetozar Blazhev <nathraichean@abv.bg>
2020-03-09 23:55:32 +0200

    JavaFund>ObjClasses>Lab>06.Completed

--- Java Fundamentals/6. Objects and Classes/Lab/5. Students/src/Main.java
+++ Java Fundamentals/6. Objects and Classes/Lab/5. Students/src/Main.java
@@ -7,6 +7,7 @@
  */
 
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -16,5 +17,33 @@ public class Main {
         //init
         Scanner scanner = new Scanner(System.in);
 
+        ArrayList<Student> studentsList = new ArrayList<>();
+        boolean end = false;
+
+        while (!end){
+
+            String[] input = scanner.nextLine().trim().split(" ");
+
+            if (input[0].equals("end")){
+                end = true;
+
+                String hometown = scanner.nextLine();
+
+                for (Student student:studentsList) {
+                    if (student.getHomeTown().equals(hometown)){
+                        System.out.printf("%s %s is %d years old\n",student.getFirstName(),student.getLastName(),student.getAge());
+                    }
+                }
+
+            }
+            else if (input.length == 4){
+                Student student = new Student(input[0],input[1],Integer.parseInt(input[2]),input[3]);
+                studentsList.add(student);
+            }
+
+
+        }
+
     }
+
 }

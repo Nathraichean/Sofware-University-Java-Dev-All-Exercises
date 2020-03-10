4e341166b79b35065993bd1444b12607eb9f197f
Svetozar Blazhev <nathraichean@abv.bg>
2020-03-10 13:14:14 +0200

    JavaFund>ObjCLasses>Exercise>03.Completed

--- Java Fundamentals/6. Objects and Classes/Excercise/3. Opinion Poll/src/Main.java
+++ Java Fundamentals/6. Objects and Classes/Excercise/3. Opinion Poll/src/Main.java
@@ -7,6 +7,9 @@
  */
 
 
+import java.net.Inet4Address;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class Main {
@@ -16,5 +19,41 @@ public class Main {
         //init
         Scanner scanner = new Scanner(System.in);
 
+        int n = Integer.parseInt(scanner.nextLine());
+        ArrayList<Person> peopleList = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            String[] input = scanner.nextLine().split(" ");
+            peopleList.add(new Person(input[0], Integer.parseInt(input[1])));
+        }
+
+        peopleList.stream().filter(person -> person.getAge() > 30)
+                            .sorted(Comparator.comparing(Person::getName))
+                            .forEach(person -> System.out.printf("%s - %s\n",person.getName(),person.getAge()));
+
+    }
+
+    public static class Person{
+        private String name;
+        private int age;
+
+        public Person(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+        public void setAge(int age) {
+            this.age = age;
+        }
     }
 }

5656ca821e9912e745885c4bee74708aed3fad3e
Svetozar Blazhev <nathraichean@abv.bg>
2020-03-10 11:50:36 +0200

    JavaFund>ObjClasses>Exercise>01.Completed

--- Java Fundamentals/6. Objects and Classes/Excercise/1. Advertisement Message/src/Main.java
+++ Java Fundamentals/6. Objects and Classes/Excercise/1. Advertisement Message/src/Main.java
@@ -7,6 +7,7 @@
  */
 
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
@@ -16,5 +17,41 @@ public class Main {
         //init
         Scanner scanner = new Scanner(System.in);
 
+        MessageManager messageManager = new MessageManager();
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 0; i < n; i++) {
+            messageManager.printMessage();
+        }
+
+    }
+
+    public static class MessageManager {
+
+        private String[] phrases = {"Excellent product.", "Such a great product.", "I always use that product.",
+                "Best product of its category.", "Exceptional product.", "I can’t live without this product."};
+
+        private String[] events = {"Now I feel good.", "I have succeeded with this product.",
+                "Makes miracles. I am happy of the results!", "I cannot believe but now I feel awesome.",
+                "Try it yourself, I am very satisfied.", "I feel great!"};
+
+        private String[] authors = {"Diana", "Petya", "Stella", "Elena", "Katya", "Iva", "Annie", "Eva"};
+        private String[] cities = {"Burgas", "Sofia", "Plovdiv", "Varna", "Ruse"};
+
+        public MessageManager(){};
+
+        public void printMessage(){
+            Random rand = new Random();
+
+            int randomPhraseIndex = rand.nextInt(this.phrases.length);
+            int randomEventIndex = rand.nextInt(this.events.length);
+            int randomAuthorIndex = rand.nextInt(this.authors.length);
+            int randomCityIndex = rand.nextInt(this.cities.length);
+
+            System.out.printf("%s %s %s - %s\n", this.phrases[randomPhraseIndex],this.events[randomEventIndex],
+                                                this.authors[randomAuthorIndex],this.cities[randomCityIndex]);
+
+        }
     }
 }

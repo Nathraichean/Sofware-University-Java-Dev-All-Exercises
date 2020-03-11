dd75a5b35e6bcda74cc02b58535e47398b60f6fd
Svetozar Blazhev <nathraichean@abv.bg>
2020-03-11 18:17:46 +0200

    JavaFund>ObjClasses>Exercise>6.Completed
    Last exercise of Objects and Classes.

--- Java Fundamentals/6. Objects and Classes/Excercise/7. Order by Age/src/Main.java
+++ Java Fundamentals/6. Objects and Classes/Excercise/7. Order by Age/src/Main.java
@@ -7,7 +7,10 @@
  */
 
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -16,5 +19,55 @@ public class Main {
         //init
         Scanner scanner = new Scanner(System.in);
 
+        ArrayList<Person> peopleList = new ArrayList<>();
+
+        while (true){
+            String[] studentInput = scanner.nextLine().split("\\s+");
+
+            if (studentInput[0].equals("End")){
+                break;
+            }
+
+            peopleList.add(new Person(studentInput[0],studentInput[1],Integer.parseInt(studentInput[2])));
+        }
+
+        peopleList.stream()
+                .sorted(Comparator.comparingDouble(Person::getAge))
+                .forEach(person -> System.out.printf("%s with ID: %s is %d years old.\n",person.getFirstName(),person.getId(),person.getAge()));
+
+    }
+
+    public static class Person{
+        private String firstName;
+        private String id;
+        private int age;
+
+        public Person(String firstName, String id, int age) {
+            this.firstName = firstName;
+            this.id = id;
+            this.age = age;
+        }
+
+        public String getFirstName() {
+            return firstName;
+        }
+        public void setFirstName(String firstName) {
+            this.firstName = firstName;
+        }
+
+        public String getId() {
+            return id;
+        }
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public int getAge() {
+            return age;
+        }
+        public void setAge(int age) {
+            this.age = age;
+        }
     }
 }
+

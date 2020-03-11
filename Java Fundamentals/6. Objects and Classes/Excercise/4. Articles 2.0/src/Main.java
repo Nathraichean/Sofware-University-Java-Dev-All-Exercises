999d770f7969340c5993ed936607ed3665956e25
Svetozar Blazhev <nathraichean@abv.bg>
2020-03-11 17:22:03 +0200

    JavaFund>ObjClasses>Exercise>04. and 05. Completed

--- Java Fundamentals/6. Objects and Classes/Excercise/4. Articles 2.0/src/Main.java
+++ Java Fundamentals/6. Objects and Classes/Excercise/4. Articles 2.0/src/Main.java
@@ -7,6 +7,9 @@
  */
 
 
+import java.net.Inet4Address;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class Main {
@@ -16,5 +19,75 @@ public class Main {
         //init
         Scanner scanner = new Scanner(System.in);
 
+        ArrayList<Article> articleList = new ArrayList<>();
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 0; i < n; i++) {
+            String[] input = scanner.nextLine().trim().split(", ");
+            Article article = new Article(input[0],input[1],input[2]);
+            articleList.add(article);
+        }
+
+        String command = scanner.nextLine();
+
+        switch (command){
+
+            case "title":
+                articleList.stream()
+                            .sorted(Comparator.comparing(Article::getTitle))
+                            .forEach(article -> System.out.printf("%s - %s: %s\n",article.getTitle(),article.getContent(),article.getAuthor()));
+                break;
+            case "content":
+                articleList.stream()
+                            .sorted(Comparator.comparing(Article::getContent))
+                            .forEach(article -> System.out.printf("%s - %s: %s\n",article.getTitle(),article.getContent(),article.getAuthor()));
+                break;
+            case "author":
+                articleList.stream()
+                            .sorted(Comparator.comparing(Article::getAuthor))
+                            .forEach(article -> System.out.printf("%s - %s: %s\n",article.getTitle(),article.getContent(),article.getAuthor()));
+                break;
+        }
+
+    }
+
+    public static class Article {
+        private String title;
+        private String content;
+        private String author;
+
+        public Article(){}
+        public Article(String title, String content, String author) {
+            this.title = title;
+            this.content = content;
+            this.author = author;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("%s - %s: %s", this.title,this.content,this.author);
+        }
+
+        public String getTitle() {
+            return title;
+        }
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public String getContent() {
+            return content;
+        }
+        public void setContent(String content) {
+            this.content = content;
+        }
+
+        public String getAuthor() {
+            return author;
+        }
+        public void setAuthor(String author) {
+            this.author = author;
+        }
     }
 }
--- Java Fundamentals/6. Objects and Classes/Excercise/5. Students (excr)/src/Main.java
+++ Java Fundamentals/6. Objects and Classes/Excercise/5. Students (excr)/src/Main.java
@@ -7,7 +7,10 @@
  */
 
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -16,5 +19,52 @@ public class Main {
         //init
         Scanner scanner = new Scanner(System.in);
 
+        int numOfStudents = Integer.parseInt(scanner.nextLine());
+        ArrayList<Student> studentList = new ArrayList<>();
+
+        for (int i = 0; i < numOfStudents; i++) {
+
+            String[] studentInput = scanner.nextLine().split("\\s+");
+            studentList.add(new Student(studentInput[0],studentInput[1],Double.parseDouble(studentInput[2])));
+        }
+
+        studentList.stream()
+                    .sorted(Comparator.comparingDouble(Student::getGrade).reversed())
+                    .forEach(student -> System.out.printf("%s %s: %.2f\n",student.getFirstName(),student.getLastName(),student.getGrade()));
+
+    }
+
+    public static class Student{
+        private String firstName;
+        private String lastName;
+        private double grade;
+
+        public Student(String firstName, String lastName, double grade) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.grade = grade;
+        }
+
+        public String getFirstName() {
+            return firstName;
+        }
+        public void setFirstName(String firstName) {
+            this.firstName = firstName;
+        }
+
+        public String getLastName() {
+            return lastName;
+        }
+        public void setLastName(String lastName) {
+            this.lastName = lastName;
+        }
+
+        public double getGrade() {
+            return grade;
+        }
+        public void setGrade(double grade) {
+            this.grade = grade;
+        }
     }
 }
+

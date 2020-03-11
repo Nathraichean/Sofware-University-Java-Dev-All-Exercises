ff5b981c6e8b4aec95429ed4a5c903a7e80815f8
Svetozar Blazhev <nathraichean@abv.bg>
2020-03-11 21:11:23 +0200

    JavaFund>Maps,Lambda,Stream>Lab>02.Completed

--- Java Fundamentals/7. Maps, Lambda and Stream API/Lab/2. Word Synonyms/src/Main.java
+++ Java Fundamentals/7. Maps, Lambda and Stream API/Lab/2. Word Synonyms/src/Main.java
@@ -7,6 +7,13 @@
  */
 
 
+import jdk.jfr.Category;
+
+import java.sql.SQLOutput;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
@@ -16,5 +23,22 @@ public class Main {
         //init
         Scanner scanner = new Scanner(System.in);
 
+        int n = Integer.parseInt(scanner.nextLine());
+
+        LinkedHashMap<String, ArrayList<String>> words = new LinkedHashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            String word = scanner.nextLine();
+            String synonym = scanner.nextLine();
+
+            words.putIfAbsent(word,new ArrayList<>());
+            words.get(word).add(synonym);
+        }
+
+        for (Map.Entry<String,ArrayList<String>> entry: words.entrySet()) {
+            System.out.print(entry.getKey() + " - ");
+            String listString = String.join(", ", entry.getValue());
+            System.out.println(listString);
+        }
     }
 }
